9d6880129c474ca165b2bf15b2c70f4b8f01cfc7
Sandeep Bhaskar <ssandeepachari1@gmail.com>
2025-02-08 22:24:49 +0530

    final touchup

--- frontend/src/components/Alldata.jsx
+++ frontend/src/components/Alldata.jsx
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Alldata = () => {
     const [data, setData] = useState([]);
     const [errorMessage, setErrorMessage] = useState('');
+    const navigate = useNavigate();
+    const handleNewForm = () => {
+        navigate('/');
+    };
 
     const BACKEND_URL =
         import.meta.env.MODE === 'production'
@@ -49,6 +54,10 @@ const Alldata = () => {
                     </div>
                 )}
             </div>
+            <button onClick={handleNewForm} 
+                className="w-full py-2 mt-4 bg-indigo-600 text-white font-semibold rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500">
+                    Submit Another Form
+                    </button>
         </div>
     );
 };
