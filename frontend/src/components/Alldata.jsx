6974c2acc433cde413c147d730f66dbface3b0e1
Sandeep Bhaskar <ssandeepachari1@gmail.com>
2025-02-08 22:28:54 +0530

    done

--- frontend/src/components/Alldata.jsx
+++ frontend/src/components/Alldata.jsx
@@ -53,11 +53,11 @@ const Alldata = () => {
                         </ul>
                     </div>
                 )}
-            </div>
-            <button onClick={handleNewForm} 
+                <button onClick={handleNewForm} 
                 className="w-full py-2 mt-4 bg-indigo-600 text-white font-semibold rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500">
                     Submit Another Form
-                    </button>
+                </button>
+            </div>
         </div>
     );
 };
